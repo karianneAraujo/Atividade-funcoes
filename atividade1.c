9e4d06016b26ca0536e9d04f004a024045352135
Seu Nome <kariannearaujopinheiro@gmail.com>
2025-03-30 15:23:36 -0300

    deletei comentarios

--- atividade1.c
+++ atividade1.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-// Protótipos
+
 void cabecalho(); // void sem parâmetros
 float calcularMedia(float n1, float n2); // com retorno
 void imprimirResultado(float media); // void com parâmetro
 
 int main() {
     float nota1, nota2;
-    
+
     cabecalho();
-    
+
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
     printf("Digite a segunda nota: ");
     scanf("%f", &nota2);
-    
-    // Chamando função que chama outra função
+
+
     imprimirResultado(calcularMedia(nota1, nota2));
-    
+
     return 0;
 }
 
